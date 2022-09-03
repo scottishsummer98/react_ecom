3e2d18ba2ec1499f45f39bbe33fa41edf9ff00fe
scottishsummer98 <samiremail98@gmail.com>
2022-09-03 22:27:42 +0600

    'initial'

---
+++ src/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import './Login.css'
+import { Link, useNavigate } from 'react-router-dom'
+import { auth } from './firebase'
+
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+        />
+      </Link>
+
+      <div className="login_container">
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" value={email} />
+
+          <h5>Password</h5>
+          <input type="password" value={password} />
+
+          <button type="submit" className="login_signInButton">
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button className="login_registerButton">
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAAF_b7iRFczhuISHcAfk5hhNGADNrp09M',
+  authDomain: 'react-ecom-21931.firebaseapp.com',
+  projectId: 'react-ecom-21931',
+  storageBucket: 'react-ecom-21931.appspot.com',
+  messagingSenderId: '1019605305897',
+  appId: '1:1019605305897:web:10818d4c8c5bc52379ea38',
+}
